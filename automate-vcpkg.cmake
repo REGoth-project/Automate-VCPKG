f427fb906a271ddf214cc3ca7d350dca93d4f66a
Andre Taulien <a.taulien@live.de>
2019-06-01 12:56:24 +0200

    Fix vcpkg.cmake toolchain not being applied

--- automate-vcpkg.cmake
+++ automate-vcpkg.cmake
@@ -96,6 +96,9 @@ if(NOT DEFINED ${CMAKE_TOOLCHAIN_FILE})
     # We know this wasn't set before so we need point the toolchain file to the newly found VCPKG_ROOT
     set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE STRING "")
 
+    # Just setting vcpkg.cmake as toolchain file does not seem to actually pull in the code
+    include(${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
+
     set(AUTOMATE_VCPKG_USE_SYSTEM_VCPKG OFF)
 else()
     # VCPKG_ROOT has been defined by the toolchain file already
