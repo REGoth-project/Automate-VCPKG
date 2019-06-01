56655d927a752f5a228902c33be7eb5902b2afa8
Andre Taulien <a.taulien@live.de>
2019-06-01 12:34:16 +0200

    Fix x86 packages being installed for x64 builds

--- automate-vcpkg.cmake
+++ automate-vcpkg.cmake
@@ -63,6 +63,23 @@ else()
     set(VCPKG_FALLBACK_ROOT ${CMAKE_CURRENT_BINARY_DIR}/.vcpkg CACHE STRING  "vcpkg configuration directory to use if vcpkg was not installed on the system before")
 endif()
 
+# On Windows, Vcpkg defaults to x86, even on x64 systems. If we're 
+# doing a 64-bit build, we need to fix that.
+if (WIN32)
+
+    # Since the compiler checks haven't run yet, we need to figure
+    # out the value of CMAKE_SIZEOF_VOID_P ourselfs
+
+    include(CheckTypeSize)
+    enable_language(C)
+    check_type_size("void*" SIZEOF_VOID_P BUILTIN_TYPES_ONLY)
+    
+    if (SIZEOF_VOID_P EQUAL 8)
+        message(STATUS "Using Vcpkg triplet 'x64-windows'")
+        
+        set(VCPKG_TRIPLET x64-windows)
+    endif()
+endif()
 
 # Find out whether the user supplied their own VCPKG toolchain file
 if(NOT DEFINED ${CMAKE_TOOLCHAIN_FILE})
@@ -85,6 +102,7 @@ else()
     set(AUTOMATE_VCPKG_USE_SYSTEM_VCPKG ON)
 endif()
 
+
 # Installs a new copy of Vcpkg or updates an existing one
 macro(vcpkg_bootstrap)
     _install_or_update_vcpkg()
@@ -95,15 +113,9 @@ macro(vcpkg_bootstrap)
     message(STATUS "  VCPKG_BOOTSTRAP: ${VCPKG_BOOTSTRAP}")
 endmacro()
 
-# Installs the list of packages given as parameters using Vcpkg
-macro(vcpkg_install_packages)
-    message(STATUS "Installing/Updating the following vcpkg-packages: ${ARGN}")
-    _vcpkg_install_packages(PACKAGES ${ARGN})
-endmacro()
-
 macro(_install_or_update_vcpkg)
     if(NOT EXISTS ${VCPKG_ROOT})
-        message("Cloning vcpkg in ${VCPKG_ROOT}")
+        message(STATUS "Cloning vcpkg in ${VCPKG_ROOT}")
         execute_process(COMMAND git clone https://github.com/Microsoft/vcpkg.git ${VCPKG_ROOT})
 
         # If a reproducible build is desired (and potentially old libraries are # ok), uncomment the
@@ -111,7 +123,7 @@ macro(_install_or_update_vcpkg)
         # execute_process(COMMAND git checkout 745a0aea597771a580d0b0f4886ea1e3a94dbca6 WORKING_DIRECTORY ${VCPKG_ROOT})
     else()
         # The following command has no effect if the vcpkg repository is in a detached head state.
-        message("Auto-updating vcpkg in ${VCPKG_ROOT}")
+        message(STATUS "Auto-updating vcpkg in ${VCPKG_ROOT}")
         execute_process(COMMAND git pull WORKING_DIRECTORY ${VCPKG_ROOT})
     endif()
 
@@ -138,16 +150,23 @@ macro(_install_or_update_vcpkg)
    
 endmacro()
 
-function(_vcpkg_install_packages)
-    set(options "")
-    set(oneValueArgs "")
-    set(multiValueArgs PACKAGES)
-    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+# Installs the list of packages given as parameters using Vcpkg
+macro(vcpkg_install_packages)
+    
+    # Need the given list to be space-separated
+    #string (REPLACE ";" " " PACKAGES_LIST_STR "${ARGN}")
 
-    execute_process(COMMAND ${VCPKG_EXEC} install ${ARG_PACKAGES} WORKING_DIRECTORY ${VCPKG_ROOT})
-endfunction()
+    message(STATUS "Installing/Updating the following vcpkg-packages: ${PACKAGES_LIST_STR}")
 
+    if (VCPKG_TRIPLET)
+        set(ENV{VCPKG_DEFAULT_TRIPLET} "${VCPKG_TRIPLET}")
+    endif()
 
+    execute_process(
+        COMMAND ${VCPKG_EXEC} install ${ARGN}
+        WORKING_DIRECTORY ${VCPKG_ROOT}
+        )
+endmacro()
     
 # MIT License
 # 
